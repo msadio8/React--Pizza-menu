edd45cd8146f0aec20e460e22bb52a6904d86aef
Meriam <132971614+msadio8@users.noreply.github.com>
2024-02-13 22:20:01 +1100

    change to conditional rendering

--- src/index.js
+++ src/index.js
@@ -68,14 +68,19 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
     </main>
   );
 }
@@ -105,7 +110,13 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}. We're currently open
+      {/* {new Date().toLocaleTimeString()}. We're currently open */}
+      {isOpen && (
+        <div className="order">
+          <p>We're Open until {closeHour}:00. Come visit or order online !</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
     </footer>
   );
 
